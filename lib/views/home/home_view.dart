99939e8132d8df967d4cfda70670150f6937067a
mikemuriith <mikemuriith@gmail.com>
2022-04-09 03:30:56 +0300

    organization

---
+++ lib/views/home/home_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/contants.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: Container(),
+    );
+  }
+}
