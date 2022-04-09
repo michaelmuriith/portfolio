3145bf7fba89bc2a734f2bfa6744dafc4ba534e4
mikemuriith <mikemuriith@gmail.com>
2022-04-09 17:09:38 +0300

    spacing

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/constants/colors.dart';
+import 'package:portfolio/constants/ui_helper.dart';
 import 'package:stacked/stacked.dart';
 
 import 'home_viewmodel.dart';
@@ -12,23 +13,39 @@ class HomeView extends StatelessWidget {
     return ViewModelBuilder<HomeViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
         backgroundColor: kBackgroundColor,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              model.user,
-              style: const TextStyle(color: kCaptionColor),
-            ),
-            Text(
-              model.profession,
-              style: const TextStyle(color: kDangerColor),
-            ),
-            Text(
-              model.description,
-              style: const TextStyle(color: kCaptionColor),
-            ),
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: kPrimaryColor,
+                ),
+              ),
+              verticalSpaceLarge,
+              Text(
+                model.user,
+                style: const TextStyle(color: kCaptionColor),
+              ),
+              verticalSpaceLarge,
+              Text(
+                model.profession,
+                style: const TextStyle(color: kDangerColor),
+              ),
+              verticalSpaceLarge,
+              Text(
+                model.description,
+                style: const TextStyle(color: kCaptionColor),
+              ),
+              Row(
+                children: const [],
+              )
+            ],
+          ),
         ),
       ),
       viewModelBuilder: () => HomeViewModel(),
