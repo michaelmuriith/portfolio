3fd4244a0f3392558b60370ff495e02f5e5c418a
mikemuriith <mikemuriith@gmail.com>
2022-04-09 03:45:20 +0300

    user

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -9,7 +9,9 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBackgroundColor,
-      body: Container(),
+      body: Column(
+        children: const [Text('Michael A. Wanjiru')],
+      ),
     );
   }
 }
