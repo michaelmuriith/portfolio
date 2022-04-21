48adf4555a82a9377bbb348698731b9a89de0008
mikemuriith <mikemuriith@gmail.com>
2022-04-22 00:13:32 +0300

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home">
+      <div className="logo"></div>
+      <div className="line"></div>
+      <div className="title">
+        <h5>Michael Wanjiru</h5>
+      </div>
+      <div className="icon_btn"></div>
     </div>
   );
 }
